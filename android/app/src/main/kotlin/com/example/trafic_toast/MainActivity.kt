ef402e5654b672db753049d99699a848bd016ea7
Kyuhyeon <kyuhyeon.kim@doosan.com>
2024-03-19 15:17:51 +0900

    FEAT: codebase

---
+++ android/app/src/main/kotlin/com/example/trafic_toast/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.trafic_toast
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
